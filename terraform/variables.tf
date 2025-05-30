a1bcacab87d27b5f789719cea16afc9e3dde19b1
Krishna <35922046+posttokrish@users.noreply.github.com>
2025-05-30 11:43:47 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,18 @@
-variable "aft_management_account_id" {
+# terraform/variables.tf
+
+variable "ct_home_region" {
   type        = string
-  description = "AFT Management Account ID"
+  description = "Control Tower home region"
+  default     = "us-east-1"
+}
+
+variable "aft_admin_role_arn" {
+  type        = string
+  description = "AFT Admin Role ARN"
+}
+
+variable "account_request_table_name" {
+  type        = string
+  description = "DynamoDB table for account requests"
+  default     = "aft-request-metadata"
 }
