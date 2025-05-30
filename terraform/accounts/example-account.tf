6d5a1cc2e3d7358c9f2443fc518388aa03bb9424
Krishna <35922046+posttokrish@users.noreply.github.com>
2025-05-30 00:54:57 -0400

    Create example-account.tf

---
+++ terraform/accounts/example-account.tf
@@ -0,0 +1,23 @@
+module "example_dev_account" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory//modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "example+hkrigana@amazon.com"
+    AccountName  = "example-account"
+    ManagedOrganizationalUnit = "gss-Sandbox"
+    SSOUserEmail     = "admin+hkrigana@amazon.com"
+    SSOUserFirstName = "Krishna"
+    SSOUserLastName  = "GGGG"
+  }
+
+  account_tags = {
+    Environment = "Development"
+    Owner       = "TeamA"
+    CostCenter  = "123456"
+  }
+
+  custom_fields = {
+    project     = "example"
+    department  = "engineering"
+  }
+}
