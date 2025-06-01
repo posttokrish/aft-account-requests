59445faa751edf9062a20a8d384fd8e70c08448e
Krishna <35922046+posttokrish@users.noreply.github.com>
2025-06-01 12:50:59 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   alias  = "aft_management"  # Add this line
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::${var.aft_management_account_id}:role/AWSAFTAdmin"
+    role_arn = "arn:aws:iam::746120363643:role/AWSAFTAdmin"
   }
 }
